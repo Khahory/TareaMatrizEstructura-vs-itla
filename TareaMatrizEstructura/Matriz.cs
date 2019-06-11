316d65d22d0ffa3280616481b489112db4b46db0
KhahoryITLA <20164247@itla.edu.do>
2019-06-11 00:39:35 -0400

    Commit

---
+++ TareaMatrizEstructura/Matriz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaMatrizEstructura
+{
+    //La tarea propuesta para este ejercicio es la siguiente:
+    //Elaborar una matriz de 2 filas y 5 columnas.Realizar la carga de componentes por columna(es decir primero ingresar tod
+    //a la primer columna, luego la segunda columna y así sucesivamente)
+    //Imprimir luego la matriz.
+    class Matriz
+    {
+        private int[,] matriz;
+
+        //Cargar los valores que tendra la matriz
+        public void Cargar()
+        {
+            matriz = new int[2, 5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    Console.Write("Digite el valor para la matriz: ");
+                    string linea = Console.ReadLine();
+                    matriz[c, i] = int.Parse(linea);
+                }
+            }
+        }
+
+        //Mostrar los valores de la matriz (2,5)
+        public void Mostrar()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int c = 0; c < 5; c++)
+                {
+                    Console.Write(matriz[i, c] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TareaMatrizEstructura/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TareaMatrizEstructura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matriz matriz = new Matriz();
+            matriz.Cargar();
+            matriz.Mostrar();
+        }
+
+        //Digite el valor para la matriz: 0
+        //Digite el valor para la matriz: 1
+        //Digite el valor para la matriz: 2
+        //Digite el valor para la matriz: 3
+        //Digite el valor para la matriz: 4
+        //Digite el valor para la matriz: 5
+        //Digite el valor para la matriz: 6
+        //Digite el valor para la matriz: 7
+        //Digite el valor para la matriz: 8
+        //Digite el valor para la matriz: 9
+
+        //0 2 4 6 8
+        //1 3 5 7 9
+
+
+    }
+}
